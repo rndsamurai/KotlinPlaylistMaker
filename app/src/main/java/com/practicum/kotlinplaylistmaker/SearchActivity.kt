744d3c99e70462d5dc0c7cd48c73647762564c5a
rostovsamurai <rostovsamurai@yandex.ru>
2025-05-10 15:54:04 +0300

    commit №4

--- app/src/main/java/com/practicum/kotlinplaylistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/kotlinplaylistmaker/SearchActivity.kt
@@ -1,16 +1,80 @@
 package com.practicum.kotlinplaylistmaker
 
+import android.content.Intent
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.FrameLayout
+import android.widget.ImageButton
+import android.widget.ImageView
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class SearchActivity : AppCompatActivity() {
+    private lateinit var searchEditText: EditText
+    private var searchQuery: String? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
         setContentView(R.layout.activity_search)
 
+
+        val frameLayout = findViewById<FrameLayout>(R.id.container)
+        val inputEditText = findViewById<EditText>(R.id.inputEditText)
+        val clearButton = findViewById<ImageView>(R.id.clear_text)
+
+
+            fun clearButtonVisibility(s: CharSequence?): Int {
+                return if (s.isNullOrEmpty()) {
+                    View.GONE
+                } else {
+                    View.VISIBLE
+                }
+            }
+
+            inputEditText.addTextChangedListener(object : TextWatcher {
+                override fun beforeTextChanged(
+                    s: CharSequence?,
+                    start: Int,
+                    count: Int,
+                    after: Int
+                ) {
+
+                }
+
+                override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                    clearButton.visibility = clearButtonVisibility(s)
+                    searchQuery = s?.toString()
+                }
+
+                override fun afterTextChanged(s: Editable?) {}
+            })
+            savedInstanceState?.let {
+                searchQuery = it.getString("search_query")
+                searchQuery?.let { query -> searchEditText.setText(query) }
+            }
+            clearButton.setOnClickListener {
+                inputEditText.setText("")
+            }
+            fun onSaveInstanceState(outState : Bundle) {
+                super.onSaveInstanceState(outState)
+                searchQuery?.let { outState.putString("search_query", it) }
+            }
+            fun onRestoreInstanceState(savedInstanceState: Bundle) {
+                super.onRestoreInstanceState(savedInstanceState)
+                searchQuery = savedInstanceState.getString("search_query")
+            }
         }
     }
+
+
+
+
+
+
+
--- app/src/main/java/com/practicum/kotlinplaylistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/kotlinplaylistmaker/SettingsActivity.kt
@@ -1,10 +1,14 @@
 package com.practicum.kotlinplaylistmaker
 
+import android.content.Context
+import android.content.Intent
+import android.net.Uri
 import android.os.Bundle
+import android.widget.ImageButton
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import androidx.appcompat.app.AppCompatDelegate
+import androidx.appcompat.widget.SwitchCompat
 
 class SettingsActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -12,5 +16,57 @@ class SettingsActivity : AppCompatActivity() {
         enableEdgeToEdge()
         setContentView(R.layout.activity_settings)
 
+        val button = findViewById<ImageButton>(R.id.sharebutton)
+        button.setOnClickListener {
+            val shareintent = Intent(Intent.ACTION_SEND)
+            val subject = "https://practicum.yandex.ru/learn/android-developer-plus"
+            shareintent.setType("text/plain")
+            shareintent.putExtra(Intent.EXTRA_TEXT,subject)
+            startActivity(Intent.createChooser(shareintent,"Поделиться с помощью"))
+            }
+
+        val switch = findViewById<SwitchCompat>(R.id.darkthemeswitch)
+        val sharedPreferences = getSharedPreferences("Mode", Context.MODE_PRIVATE)
+        val editor = sharedPreferences.edit()
+        val nightMode = sharedPreferences.getBoolean("night",false)
+
+        if(nightMode) {
+            switch.isChecked = true
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
         }
-    }
+
+        switch.setOnCheckedChangeListener{ buttonView, isChecked ->
+            if (!isChecked) {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                editor.putBoolean("night", false)
+                editor.apply()
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                editor.putBoolean("night", true)
+                editor.apply()
+            }
+        }
+       val email = getString(R.string.email)
+        val link = getString(R.string.link)
+        val message = getString(R.string.message)
+        val gratutude = getString(R.string.gratitude)
+
+        val onemorebutton = findViewById<ImageButton>(R.id.useragreementbutton)
+        onemorebutton.setOnClickListener{
+            val url = Uri.parse(link)
+            val intent = Intent(Intent.ACTION_VIEW, url)
+            startActivity(intent)
+            }
+        val lastbutton = findViewById<ImageButton>(R.id.supportbutton)
+        lastbutton.setOnClickListener{
+            val sendintent = Intent(Intent.ACTION_SENDTO)
+            sendintent.data = Uri.parse("mailto:")
+            sendintent.putExtra(Intent.EXTRA_EMAIL, arrayOf(email))
+            sendintent.putExtra(Intent.EXTRA_SUBJECT, gratutude)
+            sendintent.putExtra(Intent.EXTRA_TEXT, message)
+            startActivity(sendintent)
+            }
+        }
+
+
+}
